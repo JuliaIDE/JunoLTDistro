aba62ffda0c4e74c60531c212d98bf97dc5938f2
Mike Innes <mike.j.innes@gmail.com>
2014-12-07 21:13:15 +0000

    fix loading page

--- build.jl
+++ build.jl
@@ -105,7 +105,7 @@ function appnw(folder)
   copy("deps/LightTable/deploy/settings", folder)
   copy("package.json", "$folder/package.json")
   copy("Juno.html", "$folder/core/Juno.html")
-  copy("juno.png", "$folder/core/juno.jpg")
+  copy("juno.png", "$folder/core/juno.png")
   copy("icons/icon.png", "$folder/core/img/icon.png")
 
   for (plugin, _) in plugins
