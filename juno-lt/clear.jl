70ee6e8aed4d313886f7073efb50d8d9407f71a0
Mike Innes <mike.j.innes@gmail.com>
2014-12-12 19:56:59 +0000

    update name

--- juno-lt/clear.jl
+++ juno-lt/clear.jl
@@ -1,5 +1,5 @@
 #!/usr/local/bin/julia
 
-@osx_only settings = "$(homedir())/Library/Application Support/Juno"
+@osx_only settings = "$(homedir())/Library/Application Support/Juno-LT"
 
 isdir(settings) && rm(settings, recursive = true)
