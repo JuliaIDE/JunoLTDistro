340760fd54dccb987a37be759f1a6bef38f8f008
Mike Innes <mike.j.innes@gmail.com>
2014-12-23 03:19:11 +0000

    new upload script

--- juno-lt/upload.jl
+++ juno-lt/upload.jl
@@ -1,9 +1,11 @@
 #!/usr/local/bin/julia
 
-function upload(f)
+function upload(f, target)
   println(f)
-  `aws put junolab/$(basename(f)) $f --public --progress` |> run
+  `aws put junolab/$target $f --public --progress` |> run
 end
 
-upload("dist/juno.dmg")
-upload("dist/windows.zip")
+upload("dist/juno.dmg", "latest/juno-mac-x64.dmg")
+for plat in ["windows", "linux"], a in ["32", "64"]
+  upload("dist/$plat$a.zip", "latest/juno-$plat-x$a.zip")
+end
