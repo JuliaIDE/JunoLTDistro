9427fe93d6cb25503c1931bb1e75916d8a4f5f7b
Mike Innes <mike.j.innes@gmail.com>
2015-03-11 15:31:31 +0000

    add release script

---
+++ juno-lt/release.jl
@@ -0,0 +1,19 @@
+#!/usr/local/bin/julia
+
+@assert length(ARGS) == 1 "Version number required"
+
+version = ARGS[1]
+
+function release(name)
+  println(name)
+  to = replace(basename(name), "-signed", "")
+  run(`aws cp junolab/release/$version/$to /junolab/latest/$name`)
+  run(`aws put junolab/release/$version/$to?acl --public`)
+end
+
+release("signed/juno-mac-x64-signed.dmg")
+
+for arch = ["x32", "x64"]
+  release("juno-linux-$arch.zip")
+  release("juno-windows-$arch.zip")
+end
